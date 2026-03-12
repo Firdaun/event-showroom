0c951d48636846e7b13c501153a0812dba3a46da
Firdaun <ketuakls776@gmail.com>
2026-03-13 04:52:32 +0700

    feat: initialize project with package.json and basic structure
    - Added package.json with dependencies (axios, cors, express)
    - Created public directory with Index.html, Script.js, and Style.css files

---
+++ main.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+import axios from 'axios'
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.static('public'))
+
+app.get('/api/ranking', async (req, res) => {
+    try {
+        console.log('Sedang mengambil data dari showroom')
+        const response = await axios.get('https://www.showroom-live.com/api/events/41907/ranking?room_id=318118')
+        res.json(response.data)
+    } catch (e) {
+        console.error("Gagal mengambil data:", e.message)
+        res.status(500).json({ e: "Gagal mengambil data" })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server berjalan di http://localhost:${port}`)
+})
